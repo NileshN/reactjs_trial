73a916a650d45543f00f8da417ff00ac939ee1bf
NileshN <navale.nilesh@gmail.com>
2017-06-23 17:19:38 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var config = {
+	entry: './main.js',
+
+	output: {
+		path: './',
+		filename: 'main.js',
+	},
+
+	devServer: {
+		inline: true,
+		port: 8000
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+
+				query:{
+					presets: ['es2015', 'react']
+				}	
+			}	
+		]
+	}
+}
